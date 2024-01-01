e5629799404d9e90357a92106bd477bc67416aee
SNUPI <joohaem@daum.net>
2024-01-01 18:24:48 +0900

    Adjust Pretendard with next font

---
+++ src/app/const.ts
@@ -0,0 +1,8 @@
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+// function assertString(data: unknown) {
+//   if (typeof data !== "string") {
+//     throw new Error("ENV NOT SET");
+//   }
+//   return data;
+// }
