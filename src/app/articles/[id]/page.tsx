cae4232cefb77f0b5b38625881c62c3af3a88070
Joohaem Lee <joohaem.lee@tridge.com>
2024-01-28 21:27:11 +0900

    Add meta data on article page

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,9 +1,36 @@
 import ArticlePage from '@/components/ArticlePage';
+import { getArticleById, getArticles } from '@/core/blog';
+import { Metadata, ResolvingMetadata } from 'next';
 
 interface ArticleProps {
   params: {
     id: string
   }
+}
+
+export async function generateStaticParams() {
+  const articles = await getArticles();
+
+  return articles
+    .map((article) => ({
+      id: article.id,
+    }));
+}
+
+export async function generateMetadata({ params }: ArticleProps, _parent: ResolvingMetadata): Promise<Metadata> {
+  const id = params.id;
+  const article = await getArticleById(id);
+
+  return {
+    title: article.title,
+    description: 'SNUPI Blog',
+    openGraph: {
+      title: `${article.title} | SNUPI Blog`,
+      description: 'SNUPI Blog',
+      type: 'article',
+      images: article.thumbnail ?? '',
+    },
+  };
 }
 
 async function Article({ params }: ArticleProps) {
