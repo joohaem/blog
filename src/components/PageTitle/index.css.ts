aa24289efe7e481bbbac1b1321a1cbeaca392815
SNUPI <joohaem@daum.net>
2024-02-17 22:14:39 +0900

    Fix cascading issue between classNames

--- src/components/PageTitle/index.css.ts
+++ src/components/PageTitle/index.css.ts
@@ -23,7 +23,7 @@ export const container = style({
       justifyContent: "normal",
       alignItems: "flex-end",
 
-      paddingBottom: "48px",
+      paddingBottom: "48px !important",
     },
   },
 });
