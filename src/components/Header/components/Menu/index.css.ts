d0ba027067347ae66af6dfe8e4ec7d583a78de41
joohaem-lee <joohaem.lee@toss.im>
2024-06-06 13:33:38 +0900

    Fix menu mobile backdrop opcaity value

--- src/components/Header/components/Menu/index.css.ts
+++ src/components/Header/components/Menu/index.css.ts
@@ -10,7 +10,7 @@ export const backdropContainer = style({
   left: "0",
   right: "0",
   bottom: "0",
-  backgroundColor: "rgba(1, 1, 1, 0.5)",
+  backgroundColor: "rgba(1, 1, 1, 0.7)",
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
